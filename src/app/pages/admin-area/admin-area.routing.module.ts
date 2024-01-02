b0c55c090711682dc3cdb8c09f79207f93c7c101
ekirkizheva <kirkizhevaelena@gmail.com>
2024-01-02 14:37:03 +1000

    update admin routing

--- src/app/pages/admin-area/admin-area.routing.module.ts
+++ src/app/pages/admin-area/admin-area.routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChangeLogComponent } from './change-log/change-log.component';
 import { HomeComponent } from './home/home.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
+import { ManageUsersComponent } from './manage-users/manage-users.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'manage-products', component: ManageProductsComponent},
+    { path: 'manage-users', component: ManageUsersComponent},
     { path: 'change-log', component: ChangeLogComponent}
 ]
 @NgModule({
