16a4a07b46f57d153a5c250b37059b5b16d2265e
ekirkizheva <kirkizhevaelena@gmail.com>
2024-01-05 15:42:16 +1000

    Modify feature faciliy

--- src/app/pages/admin-area/manage-products-modify/manage-products-modify.component.ts
+++ src/app/pages/admin-area/manage-products-modify/manage-products-modify.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ManageProductsModifyService } from './manage-products-modify.service';
 
 @Component({
   selector: 'app-manage-products-modify',
   templateUrl: './manage-products-modify.component.html',
   styleUrls: ['./manage-products-modify.component.scss']
 })
-export class ManageProductsModifyComponent {
+export class ManageProductsModifyComponent implements OnInit {
 
   error = '';
 
   productId;
 
+  deleteConfirmationPending: {[key:number]: boolean} = {};
+
+  isNewFeatureVisible = false;
+  
   productForm = this.fb.group({
     product_name: ['', Validators.required],
     product_model: ['', Validators.required],
@@ -22,19 +27,63 @@ export class ManageProductsModifyComponent {
     features: [[], Validators.required]
   });
 
+  newFeatureForm = this.fb.group({
+    weight: ['', Validators.required],
+    dimensions: ['', Validators.required],
+    os: ['', Validators.required],
+    screensize: ['', Validators.required],
+    resolution: ['', Validators.required],
+    cpu: ['', Validators.required],
+    ram: ['', Validators.required],
+    storage: ['', Validators.required],
+    battery: ['', Validators.required],
+    rear_camera: ['', Validators.required],
+    front_camera: ['', Validators.required],
+  });
+
   constructor(
     private fb: UntypedFormBuilder,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private manageProductsModifyService: ManageProductsModifyService
   ) {
     this.productId = this.route.snapshot.paramMap.get('id');
+  }
+
+  ngOnInit(): void {
+    if (this.productId) {
+      this.manageProductsModifyService.getProduct(+this.productId).subscribe((result) => {
+        this.productForm.patchValue({
+          ...result
+        })
+      })
+    }
   }
 
   back() {
     this.router.navigate(['admin/manage-products']);
   }
 
+  confirmDelete(id:number) {
+    this.deleteConfirmationPending[id] = true;
+  }
+
+  cancelDelete(id:number) {
+    this.deleteConfirmationPending[id] = false;
+  }
+
+  deleteFeature(id:number) {
+
+  }
+
   onSubmit() {
-    
+    console.log('sumbit called');
+  }
+
+  addFeature() {
+    this.isNewFeatureVisible = false;
+    this.productForm.get('features')?.value.push(this.newFeatureForm.value);
+    this.newFeatureForm.reset();
+    console.log('add feature called');
   }
 }
--- src/app/pages/admin-area/manage-products-modify/manage-products-modify.service.ts
+++ src/app/pages/admin-area/manage-products-modify/manage-products-modify.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/shared/models/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManageProductsModifyService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getProduct(id:number): Observable<Product> {
+    return this.http.get<Product>(`/api/public/product/${id}`);
+  }
 }
